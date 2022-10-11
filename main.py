a51dc3a83e8dfa64c30a0d7113b15e8c90aa8758
clotilde <liautard.clotilde@gmail.com>
2022-10-11 21:20:57 +0200

    k

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+from random import *
+
+#Fonction qui transforme un nombre en liste
+def Nombre_Liste (nombre):
+    x = []
+    longueur_nombre = len(str(nombre))
+    while longueur_nombre != 0:
+        longueur_nombre = longueur_nombre - 1
+        x.append(int(str(nombre)[longueur_nombre]))
+    return x
