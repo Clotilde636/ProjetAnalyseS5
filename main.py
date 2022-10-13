8346aa9842c0690d6f443e4fb30ace0c5db9d0e3
clotilde <liautard.clotilde@gmail.com>
2022-10-13 11:48:58 +0200

    k

--- main.py
+++ main.py
@@ -9,4 +9,12 @@ def Nombre_Liste (nombre):
         longueur_nombre = longueur_nombre - 1
         x.append(int(str(nombre)[longueur_nombre]))
     return x
+
+# [int(random()*10) for i in range (n)] pour avoir un nombre aléatoire de taille n
+
+#1. Algorithme naïf
+
+#Soient x,y deux nombres de taille n
+def Multiplication_Ecoleprimaire (x,y):
+    n = len(str(x))
 print ("test")
