8c0e0f090036096cd7d27de47bf38d7c0b5ad486
ugo-bonnet <80956091+ugo-bonnet@users.noreply.github.com>
2022-10-30 20:47:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,28 @@ def Multiplication_Ecoleprimaire (x,y):
         for j in range (t2):
           nb1_liste[i]*nb2_liste[j]  
 
+# polynome 8 + 8x + 2x² + 0x^3 + 4x^4 + 5x^5
+liste_P = [8,8,2,0,4,5]
 
+def separation_polynome_paire_impaire(liste_P):
+    liste_P_paire = []
+    liste_P_impaire = []
+    liste_P_finale = [] #liste de liste avec les deux du dessus
+
+    for i in range (0, len(liste_P)-1, 2):
+        liste_P_paire.append(liste_P[i])
+        print(liste_P_paire)
+        liste_P_impaire.append(liste_P[i+1])
+        print(liste_P_impaire)
+    
+    liste_P_finale.append(liste_P_paire)
+    liste_P_finale.append(liste_P_impaire)
+
+    return liste_P_finale
 
 
 ############################################ EXECUTION #############################################
 
-print (Multiplication_Ecoleprimaire(3,4))
+#print (Multiplication_Ecoleprimaire(3,4))
+
+print(separation_polynome_paire_impaire(liste_P))
